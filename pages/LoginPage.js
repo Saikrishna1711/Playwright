e8f7a64b19863c233ca8895df58e832a8eb82156
Saikrishna1711 <ampatisaikrishna17@gmail.com>
2026-06-23 20:15:54 +0530

    decouple elements from test and parameterize test data

---
+++ pages/LoginPage.js
@@ -0,0 +1,44 @@
+import { test } from '@playwright/test';
+
+class LoginPage{
+
+    constructor(page){
+        this.page = page;
+        this.loginHeader = page.locator(".orangehrm-login-slot h5");
+        this.userNameInput = page.locator("//input[@name='username']");
+        this.passwordInput = page.locator("//input[@name='password']");
+        this.submitBtn = page.locator("//button[@type='submit']");
+        this.dashboard = page.locator("header h6");
+        this.requiredMessage = page.locator("span");
+        this.invalidCredentials = page.locator(".orangehrm-login-error div p");
+        this.forgotPasswordBtn = page.locator(".orangehrm-login-forgot");
+        this.resetPasswordHeader = page.locator(".orangehrm-card-container h6");
+        this.resetPasswordUserNameInput = page.getByPlaceholder("Username");
+        this.resetPasswordCancelBtn = page.locator("//button[@type='button']")
+        this.officialPagesIcon = page.locator("div a svg");
+
+    }
+    
+
+    async navigateTo(url){
+        await this.page.goto(url);
+        await this.page.waitForLoadState();
+    }
+
+    async validateLogin(url, username, password){
+        await this.page.goto(url)
+        await this.userNameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.submitBtn.click();
+    }
+
+    async navigateToOfficialPage(context, officialPages, i){
+    
+        const [newPage] = await Promise.all([context.waitForEvent('page'), officialPages.nth(i).click()]);
+        const title = await newPage.title();
+        await newPage.close();
+        return title;
+      
+    }
+}
+module.exports = {LoginPage};
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,100 +1,105 @@
 import { test, expect } from '@playwright/test';
-import { off } from 'node:cluster';
-import { parentPort } from 'node:worker_threads';
+import { LoginPage } from '../pages/LoginPage';
+import LoginData from '../test_data/loginTestData.json';
+
+
 const url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
 
 test("Verify Login page", async ({ page }) => {
-    await page.goto(url);
-    await expect(page).toHaveTitle("OrangeHRM");
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateTo(url);
+    await expect(loginPage.page).toHaveTitle("OrangeHRM");
 });
 
 
 test("Verify Login with valid credentials", async ({ page }) => {
-    await page.goto(url);
-    await page.locator("//input[@name='username']").fill("Admin");
-    await page.locator("//input[@name='password']").fill("admin123");
-    await page.locator("//button[@type='submit']").click();
-
-    await expect(page.locator("header h6")).toHaveText("Dashboard");
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.validateLogin(url, LoginData.validLoginData.username, LoginData.validLoginData.password);
+    await expect(loginPage.dashboard).toHaveText("Dashboard");
 });
 
 test("Verify Login without Credentials" , async ({page}) => {
-    await page.goto(url);
-    await page.locator("//button[@type='submit']").click();
-    await expect(page.locator("span").first()).toBeVisible();
-     await expect(page.locator("span").first()).toHaveText("Required");
+    const loginPage = new LoginPage(page);
+
+    await loginPage.validateLogin(url, LoginData.emptyLoginData.username, LoginData.emptyLoginData.password);
+    
+    await expect(loginPage.requiredMessage.first()).toBeVisible();
+    await expect(loginPage.requiredMessage.first()).toHaveText("Required");
 });
 
-test("Verify login with invalid credentials" , async ({page}) => {
-    await page.goto(url);
-    await page.locator("//input[@name='username']").fill("invalidUsername");
-    await page.locator("//input[@name='password']").fill("invalidpassword");
-    await page.locator("//button[@type='submit']").click();
+test.describe("Invalid Login Scenarios",()=>{
+    for(const invalidData of LoginData.invalidLoginData){
+        test(`Verify login with invalid username: ${invalidData.username} and password: ${invalidData.password}`, async ({page}) => {
+            const loginPage = new LoginPage(page);
 
-    await expect(page.locator(".orangehrm-login-error div p").first()).toBeVisible();
-    await expect(page.locator(".orangehrm-login-error div p").first()).toHaveText("Invalid credentials");
-
+            await loginPage.validateLogin(url, invalidData.username, invalidData.password);
     
+            await expect(loginPage.invalidCredentials.first()).toBeVisible();
+            await expect(loginPage.invalidCredentials.first()).toHaveText("Invalid credentials");
+        });
+    }
+
 });
 
 test("Verify forgot password successflow flow", async ({page}) => {
-    await page.goto(url);
-    await page.locator(".orangehrm-login-forgot").click();
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.navigateTo(url);
+    await loginPage.forgotPasswordBtn.click();
+    await expect(loginPage.resetPasswordHeader).toHaveText("Reset Password");
 
-    await expect(page.locator(".orangehrm-card-container h6")).toHaveText("Reset Password");
-    await expect(page.getByPlaceholder("Username")).toBeVisible();
+    await loginPage.resetPasswordUserNameInput.fill(LoginData.validLoginData.username);
+    await loginPage.submitBtn.click();
 
-    await page.getByPlaceholder("Username").fill("Admin");
-    await page.locator("//button[@type='submit']").click();
-
-    await expect(page.locator(".orangehrm-card-container h6")).toBeVisible();
-    await expect(page.locator(".orangehrm-card-container h6")).toHaveText("Reset Password link sent successfully");  
+    await expect(loginPage.resetPasswordHeader).toBeVisible();
+    await expect(loginPage.resetPasswordHeader).toHaveText("Reset Password link sent successfully");  
 });
 
 test("Forgot Password invalid flow", async ({page}) => {
-    await page.goto(url);
-    await page.locator(".orangehrm-login-forgot").click();
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.navigateTo(url);
+    await loginPage.forgotPasswordBtn.click();
 
-    await expect(page.locator(".orangehrm-card-container h6")).toHaveText("Reset Password");
-    await expect(page.getByPlaceholder("Username")).toBeVisible();
+    await expect(loginPage.resetPasswordHeader).toHaveText("Reset Password");
+    await expect(loginPage.resetPasswordHeader).toBeVisible();
 
-    await page.locator("//button[@type='submit']").click();
-    await expect(page.locator("span")).toBeVisible();
-    await expect(page.locator("span")).toHaveText("Required");
+    await loginPage.submitBtn.click();
+    await expect(loginPage.requiredMessage).toBeVisible();
+    await expect(loginPage.requiredMessage).toHaveText("Required");
 });
 
 test("Cancel Forgot password", async ({page}) => {
-    await page.goto(url);
-    await page.locator(".orangehrm-login-forgot").click();
-    await expect(page.locator(".orangehrm-card-container h6")).toHaveText("Reset Password");
-    await expect(page.getByPlaceholder("Username")).toBeVisible();
-    await page.locator("//button[@type='button']").click();
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.navigateTo(url);
+    await loginPage.forgotPasswordBtn.click();
+    await expect(loginPage.resetPasswordHeader).toHaveText("Reset Password");
+    await expect(loginPage.resetPasswordUserNameInput).toBeVisible();
+    await loginPage.resetPasswordCancelBtn.click();
 
-    await expect(page.locator(".orangehrm-login-slot h5")).toBeVisible();
-    await expect(page.locator(".orangehrm-login-slot h5")).toHaveText("Login");
+    await expect(loginPage.loginHeader).toBeVisible();
+    await expect(loginPage.loginHeader).toHaveText("Login");
 });
 
 test("Verify Navigation to Official pages", async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     const titles = ["OrangeHRM | LinkedIn", "OrangeHRM (@orangehrm) / X", "OrangeHRM Inc - YouTube"];
-    await page.goto(url);
-
-    const officialPages = await page.locator("div a svg")
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.navigateTo(url);
+    const officialPages = await loginPage.officialPagesIcon;
     await expect(officialPages.last()).toBeVisible();
     const officialPagesCount = await officialPages.count();
-
-    for(var i=0; i< officialPagesCount; i++){
+    console.log(officialPagesCount);
+    for(var i=0; i < officialPagesCount; i++){
         if(i!==1){
-            const [newPage] = await Promise.all([context.waitForEvent('page'), officialPages.nth(i).click()]);
-            const title = await newPage.title();
-            console.log("Navigating to "+ title);
+            const title = await loginPage.navigateToOfficialPage(context, officialPages, i);
+            console.log("Opened "+ title + "page");
             await expect(titles).toContain(title);
-            await newPage.close();
             console.log("Back to original page: " + await page.title());
         }
-    }
+    }    
 });
-
-
-
