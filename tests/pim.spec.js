8760469357e2dab7304ea703798690458de1784d
Saikrishna1711 <ampatisaikrishna17@gmail.com>
2026-06-25 21:46:34 +0530

    Refactor employee tests

--- tests/pim.spec.js
+++ tests/pim.spec.js
@@ -5,17 +5,19 @@ import { LoginPage } from '../pages/LoginPage';
 import LoginData from '../test_data/loginTestData.json';
 import EmployeeData from '../test_data/employeeTestData.json';
 
-const pageName = "PIM";
-
 
 test("Verify Successsful Add employee flow", async ({page, loggedIn}) => {
     const pimPage = new PimPage(page);
     const loginPage = new LoginPage(page);
-    await pimPage.navigateToPage(pageName);
+    await pimPage.navigateToPage(EmployeeData.pageNames.pimPage);
     await expect(loginPage.dashboard).toHaveText(pageName);
 
     await pimPage.employeeAddBtn.click();
-    await pimPage.addEmployee(EmployeeData.validEmployeeDetails.empFirstName, EmployeeData.validEmployeeDetails.empLastName, EmployeeData.validEmployeeDetails.empId)
+    await pimPage.addEmployee(
+        EmployeeData.validEmployeeDetails.empFirstName, 
+        EmployeeData.validEmployeeDetails.empLastName,
+        EmployeeData.validEmployeeDetails.empId
+    )
     await expect(pimPage.successToastmessage).toBeVisible();
 })
 
@@ -23,10 +25,14 @@ test("Verify Successsful Add employee flow", async ({page, loggedIn}) => {
 test("Verify add employee with empty data", async ({page, loggedIn}) => {
     const loginPage = new LoginPage(page);
     const pimPage = new PimPage(page);
-    await pimPage.navigateToPage(pageName);
-    await expect(loginPage.dashboard).toHaveText(pageName);
+    await pimPage.navigateToPage(EmployeeData.pageNames.pimPage);
+    await expect(loginPage.dashboard).toHaveText(EmployeeData.pageNames.pimPage);
     await pimPage.employeeAddBtn.click();
-    await pimPage.addEmployee(EmployeeData.emptyEmployeeDetails.empFirstName, EmployeeData.emptyEmployeeDetails.empLastName, EmployeeData.emptyEmployeeDetails.empId)
+    await pimPage.addEmployee(
+        EmployeeData.emptyEmployeeDetails.empFirstName,
+        EmployeeData.emptyEmployeeDetails.empLastName, 
+        EmployeeData.emptyEmployeeDetails.empId
+    )
     await pimPage.employeeIdInput.clear();
     await expect(pimPage.requiredMessage.first()).toHaveText("Required");
 })
@@ -35,31 +41,35 @@ test("Verify add employee with empty data", async ({page, loggedIn}) => {
 test("Verify Cancel Add employee", async ({page, loggedIn}) => {
     const loginPage = new LoginPage(page);
     const pimPage = new PimPage(page);
-    await pimPage.navigateToPage(pageName);
-    await expect(loginPage.dashboard).toHaveText(pageName);
+    await pimPage.navigateToPage(EmployeeData.pageNames.pimPage);
+    await expect(loginPage.dashboard).toHaveText(EmployeeData.pageNames.pimPage);
     await pimPage.empCancelBtn.click();
     await expect(pimPage.empSaveBtn).toBeVisible();
     
 })
 
-test.describe.only("verify employee created", async () => {
+test.describe("verify employee created", async () => {
     test("Verify employee with employeeId", async({page, loggedIn})=>{
         
         //create emp
         const pimPage = new PimPage(page);
         const loginPage = new LoginPage(page);
-        await pimPage.navigateToPage(pageName);
-        await expect(loginPage.dashboard).toHaveText(pageName);
+        await pimPage.navigateToPage(EmployeeData.pageNames.pimPage);
+        await expect(loginPage.dashboard).toHaveText(EmployeeData.pageNames.pimPage);
 
         await pimPage.employeeAddBtn.click();
-        await pimPage.addEmployee(EmployeeData.validEmployeeDetails2.empFirstName, EmployeeData.validEmployeeDetails2.empLastName, EmployeeData.validEmployeeDetails2.empId)
+        await pimPage.addEmployee(
+            EmployeeData.validEmployeeDetails2.empFirstName, 
+            EmployeeData.validEmployeeDetails2.empLastName, 
+            EmployeeData.validEmployeeDetails2.empId
+        )
         await expect(pimPage.successToastmessage).toBeVisible();
     
         //verify created employee
-        await pimPage.navigateToPage("Dashboard");
-        await expect(loginPage.dashboard).toHaveText("Dashboard");
-        await pimPage.navigateToPage(pageName);
-        await expect(loginPage.dashboard).toHaveText(pageName);
+        await pimPage.navigateToPage(EmployeeData.pageNames.dashBoardPage);
+        await expect(loginPage.dashboard).toHaveText(EmployeeData.pageNames.dashBoardPage);
+        await pimPage.navigateToPage(EmployeeData.pageNames.pimPage);
+        await expect(loginPage.dashboard).toHaveText(EmployeeData.pageNames.pimPage);
         await pimPage.filterEmpId.fill(EmployeeData.validEmployeeDetails2.empId);
         await pimPage.searchBtn.click();
 
@@ -68,35 +78,33 @@ test.describe.only("verify employee created", async () => {
         const empId =  await pimPage.empRecord.locator("div div").nth(2).innerText();
         await expect(empId).toBe(EmployeeData.validEmployeeDetails2.empId);
     })
-
-    // test("Verify employee with employee name", async({page, loggedIn}) =>{
-    //     const pimPage = new PimPage(page);
-    //     const loginPage = new LoginPage(page);
-    //     await pimPage.navigateToPage(pageName);
-    //     await expect(loginPage.dashboard).toHaveText(pageName);
-    //     await pimPage.filterEmpName.fill(EmployeeData.validEmployeeDetails.empFirstName);
-    //     await pimPage.searchBtn.click();
-
-    //     await expect(pimPage.empRecord).toBeVisible();
-    //     await expect(pimPage.empRecord.locator("div").nth(1)).toHaveText(EmployeeData.validEmployeeDetails.empId);
-    // })
-
-
-
 })
 
 test("Edit employee details", async ({page, loggedIn}) => {
-
 
 })
 
 test("Create employee with existing empid", async ({page, loggedIn}) => {
     const loginPage = new LoginPage(page);
     const pimPage = new PimPage(page);
-    await pimPage.navigateToPage(pageName);
-    await expect(loginPage.dashboard).toHaveText(pageName);
+    await pimPage.navigateToPage(EmployeeData.pageNames.pimPage);
+    await expect(loginPage.dashboard).toHaveText(EmployeeData.pageNames.pimPage);
     await pimPage.employeeAddBtn.click();
-    await pimPage.addEmployee(EmployeeData.emptyEmployeeDetails.empFirstName, EmployeeData.emptyEmployeeDetails.empLastName, EmployeeData.emptyEmployeeDetails.empId)
+    await pimPage.addEmployee(
+        EmployeeData.validEmployeeDetails3.empFirstName, 
+        EmployeeData.validEmployeeDetails3.empLastName, 
+        EmployeeData.validEmployeeDetails3.empId
+    )
+    await expect(pimPage.successToastmessage).toBeVisible();
+
+    await pimPage.navigateToPage(EmployeeData.pageNames.dashBoardPage);
+    await expect(loginPage.dashboard).toHaveText(EmployeeData.pageNames.dashBoardPage);
+    
+    await pimPage.addEmployee(
+        EmployeeData.validEmployeeDetails3.empFirstName, 
+        EmployeeData.validEmployeeDetails3.empLastName, 
+        EmployeeData.validEmployeeDetails3.empId
+    )
     await expect(pimPage.empExistsMessage).toHaveText("Employee Id already exists");
 })
 
